13957e836f80688617430c2e26d0886c42c22685
Nurainii-ok <nrainisyauq@gmail.com>
2026-04-06 09:14:09 +0700

    feat: add competition data file and remove temporary test script

--- test.js
+++
@@ -1,4 +0,0 @@
-const url = 'https://youtu.be/mUln-7dZvWI?si=0V5lLMdouJ7Fe_DS';
-const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-const match = url.match(regExp);
-console.log('Match2:', match ? match[2] : null, 'Len:', match ? match[2].length : null);
